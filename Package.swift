3a3b7d0e9f6a4b2ae481511975ab74ac8e919f45
Ankit Aggarwal <ankit_aggarwal@apple.com>
2019-09-27 20:17:51 -0700

    Re-indent Package.swift to match rest of the codebase

--- Package.swift
+++ Package.swift
@@ -2,28 +2,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-driver",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("master")),
-    ],
-    targets: [
-        /// The driver library.
-        .target(
-            name: "SwiftDriver",
-            dependencies: ["SwiftToolsSupport-auto"]),
-        .testTarget(
-            name: "SwiftDriverTests",
-            dependencies: ["SwiftDriver"]),
+  name: "swift-driver",
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("master")),
+  ],
+  targets: [
+    /// The driver library.
+    .target(
+      name: "SwiftDriver",
+      dependencies: ["SwiftToolsSupport-auto"]),
+    .testTarget(
+      name: "SwiftDriverTests",
+      dependencies: ["SwiftDriver"]),
 
-        /// The primary driver executable.
-        .target(
-            name: "driver",
-            dependencies: ["SwiftDriver"]),
+    /// The primary driver executable.
+    .target(
+      name: "driver",
+      dependencies: ["SwiftDriver"]),
 
-        /// The `makeOptions` utility (for importing option definitions).
-        .target(
-            name: "makeOptions",
-            dependencies: []),
-    ],
-    cxxLanguageStandard: .cxx14
+    /// The `makeOptions` utility (for importing option definitions).
+    .target(
+      name: "makeOptions",
+      dependencies: []),
+  ],
+  cxxLanguageStandard: .cxx14
 )
