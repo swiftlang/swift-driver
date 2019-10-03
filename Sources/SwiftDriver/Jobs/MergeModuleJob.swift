741023be1b54574ef97c8bb9a93aaa9c7e5dac9d
Ankit Aggarwal <ankit_aggarwal@apple.com>
2019-10-02 22:04:52 -0700

    Add stub job for merge module

--- Sources/SwiftDriver/Jobs/MergeModuleJob.swift
+++ Sources/SwiftDriver/Jobs/MergeModuleJob.swift
@@ -3,7 +3,13 @@ import TSCBasic
 extension Driver {
   mutating func mergeModuleJob(inputs: [InputFile]) throws -> Job {
     var commandLine: [Job.ArgTemplate] = []
+    commandLine.appendFlag("-version")
 
-    fatalError()
+    return Job(
+      tool: .absolute(try toolchain.getToolPath(.swiftCompiler)),
+      commandLine: commandLine,
+      inputs: inputs.map{ $0.file },
+      outputs: [.temporary("fake-merge-module-output.txt")]
+    )
   }
 }
