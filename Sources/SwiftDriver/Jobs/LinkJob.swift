c4e62e42fcf0a1dbbed7c6a2a77a65a9d8cac638
Harlan Haskins <harlan@apple.com>
2019-10-03 14:33:35 -0700

    [Linker] Flesh out static and dynamic linking

--- Sources/SwiftDriver/Jobs/LinkJob.swift
+++ Sources/SwiftDriver/Jobs/LinkJob.swift
@@ -1,6 +1,25 @@
 import TSCBasic
 
 extension Driver {
+  /// Returns true if the compiler depends on features provided by the ObjC
+  /// runtime that are not present on the deployment target indicated by
+  /// `triple`.
+  func wantsObjCRuntime(triple: Triple) -> Bool {
+    // When updating the versions listed here, please record the most recent
+    // feature being depended on and when it was introduced:
+    //
+    // - Make assigning 'nil' to an NSMutableDictionary subscript delete the
+    //   entry, like it does for Swift.Dictionary, rather than trap.
+    if triple.os.isiOS {
+      return triple.iOSVersion() < Triple.Version(9, 0, 0)
+    } else if triple.os.isMacOSX {
+      return triple.getMacOSXVersion().1 < Triple.Version(10, 11, 0)
+    } else if triple.os.isWatchOS {
+      return false
+    }
+    fatalError("unknown Darwin OS")
+  }
+
   /// Compute the output file for an image output.
   private func outputFileForImage(inputs: [TypedVirtualPath]) -> VirtualPath {
     // FIXME: The check for __bad__ here, is
@@ -10,6 +29,27 @@ extension Driver {
 
     let outputName = toolchain.makeLinkerOutputFilename(moduleName: moduleName, type: linkerOutputType!)
     return .relative(RelativePath(outputName))
+  }
+
+  // MARK: - Path computation
+
+  func findARCLiteLibPath() throws -> AbsolutePath? {
+    let path = try toolchain.getToolPath(.swiftCompiler)
+      .parentDirectory // 'swift'
+      .parentDirectory // 'bin'
+      .appending(components: "lib", "arc")
+
+    if localFileSystem.exists(path) { return path }
+
+    // If we don't have a 'lib/arc/' directory, find the "arclite" library
+    // relative to the Clang in the active Xcode.
+    if let clangPath = try? toolchain.getToolPath(.clang) {
+      return clangPath
+        .parentDirectory // 'clang'
+        .parentDirectory // 'bin'
+        .appending(components: "lib", "arc")
+    }
+    return nil
   }
 
   mutating func computeResourceDirPath(for triple: Triple, isShared: Bool) throws -> AbsolutePath {
@@ -32,8 +72,7 @@ extension Driver {
 
   mutating func clangLibraryPath(for triple: Triple) throws -> AbsolutePath {
     return try computeResourceDirPath(for: triple, isShared: true)
-    // Remove platform name.
-      .parentDirectory
+      .parentDirectory // Remove platform name.
       .appending(components: "clang", "lib",
                  triple.os.isDarwin ? "darwin" : triple.platformName!)
   }
@@ -46,6 +85,94 @@ extension Driver {
     }
 
     return result
+  }
+
+  // MARK: - Common argument routines
+
+  mutating func addProfileGenerationArgs(_ commandLine: inout [Job.ArgTemplate]) throws {
+    guard parsedOptions.hasArgument(.profile_generate) else { return }
+    let clangPath = try clangLibraryPath(for: targetTriple)
+
+    let runtime: String
+    if targetTriple.os.isiOS {
+      runtime = targetTriple.os.isTvOS ? "tvos" : "ios"
+    } else if targetTriple.os.isWatchOS {
+      runtime = "watchos"
+    } else {
+      assert(targetTriple.os.isMacOSX)
+      runtime = "osx"
+    }
+
+    var sim = ""
+    if targetTriple.isSimulatorEnvironment {
+      sim = "sim"
+    }
+
+    var clangRTPath = clangPath
+      .appending(component: "libclang_rt.profile_\(runtime)\(sim).a")
+
+    // FIXME: Continue accepting the old path for simulator libraries for now.
+    if targetTriple.isSimulatorEnvironment &&
+      !localFileSystem.exists(clangRTPath) {
+      clangRTPath = clangRTPath.parentDirectory
+        .appending(component: "libclang_rt.profile_\(runtime).a")
+    }
+
+    commandLine.appendPath(clangRTPath)
+  }
+
+  mutating func addDeploymentTargetArgs(_ commandLine: inout [Job.ArgTemplate]) {
+    // FIXME: Properly handle deployment targets.
+    assert(targetTriple.os.isiOS || targetTriple.os.isWatchOS || targetTriple.os.isMacOSX)
+
+    if (targetTriple.os.isiOS) {
+      if (targetTriple.os.isTvOS) {
+        if targetTriple.isSimulatorEnvironment {
+          commandLine.appendFlag("-tvos_simulator_version_min")
+        } else {
+          commandLine.appendFlag("-tvos_version_min")
+        }
+      } else {
+        if targetTriple.isSimulatorEnvironment {
+          commandLine.appendFlag("-ios_simulator_version_min")
+        } else {
+          commandLine.appendFlag("-iphoneos_version_min")
+        }
+      }
+      commandLine.appendFlag(targetTriple.iOSVersion().description)
+    } else if targetTriple.os.isWatchOS {
+      if targetTriple.isSimulatorEnvironment {
+        commandLine.appendFlag("-watchos_simulator_version_min")
+      } else {
+        commandLine.appendFlag("-watchos_version_min")
+      }
+      commandLine.appendFlag(targetTriple.watchOSVersion().description)
+    } else {
+      commandLine.appendFlag("-macosx_version_min")
+      commandLine.appendFlag(targetTriple.getMacOSXVersion().1.description)
+    }
+  }
+
+  mutating func addArgsToLinkARCLite(_ commandLine: inout [Job.ArgTemplate]) throws {
+    if !parsedOptions.hasFlag(positive: .link_objc_runtime,
+                              negative: .no_link_objc_runtime,
+                              default: wantsObjCRuntime(triple: targetTriple)) {
+      return
+    }
+
+    guard let arcLiteLibPath = try findARCLiteLibPath(),
+      let platformName = targetTriple.platformName else {
+        return
+    }
+    let fullLibPath = arcLiteLibPath
+      .appending(components: "libarclite_\(platformName).a")
+
+    commandLine.appendFlag("-force_load")
+    commandLine.appendPath(fullLibPath)
+
+    // Arclite depends on CoreFoundation.
+    commandLine.appendFlag("-framework")
+    commandLine.appendFlag("CoreFoundation")
   }
 
   mutating func addArgsToLinkStdlib(_ commandLine: inout [Job.ArgTemplate]) throws {
@@ -101,15 +228,17 @@ extension Driver {
 //        }
 //    }
 
-  // Add the runtime library link path, which is platform-specific and found
-  // relative to the compiler.
-  let runtimePaths = try runtimeLibraryPaths(for: targetTriple, isShared: true)
-  for path in runtimePaths {
-    commandLine.appendFlag("-L")
-    commandLine.appendPath(path)
-  }
+    // Add the runtime library link path, which is platform-specific and found
+    // relative to the compiler.
+    let runtimePaths = try runtimeLibraryPaths(for: targetTriple, isShared: true)
+    for path in runtimePaths {
+      commandLine.appendFlag("-L")
+      commandLine.appendPath(path)
+    }
 
-  if parsedOptions.hasArgument(.toolchain_stdlib_rpath) {
+    if parsedOptions.hasFlag(positive: .toolchain_stdlib_rpath,
+                             negative: .no_toolchain_stdlib_rpath,
+                             default: false) {
       // If the user has explicitly asked for a toolchain stdlib, we should
       // provide one using -rpath. This used to be the default behaviour but it
       // was considered annoying in at least the SwiftPM scenario (see
@@ -121,7 +250,7 @@ extension Driver {
         commandLine.appendFlag("-rpath")
         commandLine.appendPath(path)
       }
-  } else if !targetTriple.requiresRPathForSwiftInTheOS || parsedOptions.hasArgument(.no_stdlib_rpath) {
+    } else if !targetTriple.requiresRPathForSwiftInTheOS || parsedOptions.hasArgument(.no_stdlib_rpath) {
       // If targeting an OS with Swift in /usr/lib/swift, the LC_ID_DYLIB
       // install_name the stdlib will be an absolute path like
       // /usr/lib/swift/libswiftCore.dylib, and we do not need to provide an rpath
@@ -151,6 +280,8 @@ extension Driver {
     }
   }
 
+  // MARK: - Construct link job
+
   /// Link the given inputs.
   mutating func linkJob(inputs: [TypedVirtualPath]) throws -> Job {
     var commandLine: [Job.ArgTemplate] = []
@@ -162,26 +293,72 @@ extension Driver {
     //
     // Note: Normally we'd just add this unconditionally, but it's valid to build
     // Swift and use it as a linker without building compiler_rt.
-    let darwinPlatformSuffix = targetTriple.darwinLibraryNameSuffix(distinguishSimulator: false)!
-    let compilerRTPath = try clangLibraryPath(for: targetTriple)
-      .appending(component: "libclang_rt.\(darwinPlatformSuffix).a")
-    if localFileSystem.exists(compilerRTPath) {
-      commandLine.append(.path(.absolute(compilerRTPath)))
+    if let darwinPlatformSuffix =
+      targetTriple.darwinLibraryNameSuffix(distinguishSimulator: false) {
+      let compilerRTPath = try clangLibraryPath(for: targetTriple)
+        .appending(component: "libclang_rt.\(darwinPlatformSuffix).a")
+      if localFileSystem.exists(compilerRTPath) {
+        commandLine.append(.path(.absolute(compilerRTPath)))
+      }
     }
 
     // Set up for linking.
     let linkerTool: Tool
     switch linkerOutputType! {
+    case .dynamicLibrary:
+      // Same options as an executable, just with the '-dylib' flag passed
+      commandLine.appendFlag("-dylib")
+      fallthrough
     case .executable:
       linkerTool = .dynamicLinker
-      break
 
-    case .dynamicLibrary:
-      commandLine.appendFlag("-dylib")
-      linkerTool = .dynamicLinker
+      // Add the SDK path
+      if let sdkPath = sdkPath {
+        commandLine.appendFlag("-syslibroot")
+        commandLine.appendPath(try VirtualPath(path: sdkPath))
+      }
+
+      if parsedOptions.contains(.embed_bitcode) ||
+        parsedOptions.contains(.embed_bitcode_marker) {
+        commandLine.appendFlag("-bitcode_bundle")
+      }
+
+      if parsedOptions.contains(.enable_app_extension) {
+        commandLine.appendFlag("-application_extension")
+      }
+
+      let fSystemArgs = parsedOptions.filter {
+        $0.option == .F || $0.option == .Fsystem
+      }
+      for opt in fSystemArgs {
+        commandLine.appendFlag(.F)
+        commandLine.appendFlag(opt.argument.asSingle)
+      }
+
+      // FIXME: Sanitizer args
+
+      commandLine.appendFlag("-arch")
+      commandLine.appendFlag(targetTriple.archName)
+
+      commandLine.appendFlags(
+        "-lobjc",
+        "-lSystem",
+        "-no_objc_category_merging"
+      )
+
+      try addArgsToLinkStdlib(&commandLine)
+      try addArgsToLinkARCLite(&commandLine)
+      addDeploymentTargetArgs(&commandLine)
+      try addProfileGenerationArgs(&commandLine)
+
+      // These custom arguments should be right before the object file at the end.
+      try commandLine.append(
+        contentsOf: parsedOptions.filter { $0.option.group == .linker_option }
+      )
+      try commandLine.appendAllArguments(.Xlinker, from: &parsedOptions)
 
     case .staticLibrary:
-      // FIXME: handle this, somehow
+      commandLine.appendFlag("-static")
       linkerTool = .staticLinker
       break
     }
@@ -199,56 +376,6 @@ extension Driver {
       outputFile = outputFileForImage(inputs: inputs)
     }
     commandLine.appendPath(outputFile)
-
-    // Add the SDK path
-    if let sdkPath = sdkPath {
-      commandLine.appendFlag("-syslibroot")
-      commandLine.appendPath(try VirtualPath(path: sdkPath))
-    }
-
-    if parsedOptions.contains(.embed_bitcode) ||
-      parsedOptions.contains(.embed_bitcode_marker) {
-      commandLine.appendFlag("-bitcode_bundle")
-    }
-
-    if parsedOptions.contains(.enable_app_extension) {
-      commandLine.appendFlag("-application_extension");
-    }
-
-    let fSystemArgs = parsedOptions.filter {
-      $0.option == .F || $0.option == .Fsystem
-    }
-    for opt in fSystemArgs {
-      commandLine.appendFlag("-F")
-      commandLine.appendFlag(opt.argument.asSingle)
-    }
-
-    // FIXME: Sanitizer args
-
-    // FIXME: We need Triple.archName or something along those lines,
-    //        for now hard code x86_64 for testing
-    commandLine.appendFlag("-arch")
-    commandLine.appendFlag("x86_64")
-//    commandLine.appendFlag(targetTriple.archName)
-
-    commandLine.appendFlags(
-      "-lobjc",
-      "-lSystem",
-      "-no_objc_category_merging"
-    )
-
-    try addArgsToLinkStdlib(&commandLine)
-
-    // FIXME: Implement these
-    //    addArgsToLinkARCLite(Arguments, context)
-    //    addProfileGenerationArgs(Arguments, context)
-    //    addDeploymentTargetArgs(Arguments, context)
-
-    // These custom arguments should be right before the object file at the end.
-    try commandLine.append(
-      contentsOf: parsedOptions.filter { $0.option.group == .linker_option }
-    )
-    try commandLine.appendAllArguments(.Xlinker, from: &parsedOptions)
 
     return Job(
       tool: .absolute(try toolchain.getToolPath(linkerTool)),
