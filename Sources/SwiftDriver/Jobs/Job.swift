c0ef6b1786798e5352680a17ebea848878e53ef6
Owen Voorhees <owen@squibner.com>
2019-12-15 16:12:13 -0800

    Allow specifying extra environment variables on a per-job basis

--- Sources/SwiftDriver/Jobs/Job.swift
+++ Sources/SwiftDriver/Jobs/Job.swift
@@ -45,6 +45,9 @@ public struct Job: Codable, Equatable {
 
   /// The outputs produced by the job.
   public var outputs: [TypedVirtualPath]
+  
+  /// Any extra environment variables which should be set while running the job.
+  public var extraEnvironment: [String: String]
 
   /// The kind of job.
   public var kind: Kind
@@ -55,7 +58,8 @@ public struct Job: Codable, Equatable {
     commandLine: [ArgTemplate],
     displayInputs: [TypedVirtualPath]? = nil,
     inputs: [TypedVirtualPath],
-    outputs: [TypedVirtualPath]
+    outputs: [TypedVirtualPath],
+    extraEnvironment: [String: String] = [:]
   ) {
     self.kind = kind
     self.tool = tool
@@ -63,6 +67,7 @@ public struct Job: Codable, Equatable {
     self.displayInputs = displayInputs ?? []
     self.inputs = inputs
     self.outputs = outputs
+    self.extraEnvironment = extraEnvironment
   }
 }
 
@@ -77,6 +82,13 @@ extension Job: CustomStringConvertible {
         result += string.spm_shellEscaped()
       case .path(let path):
         result += path.name.spm_shellEscaped()
+      }
+    }
+
+    if !self.extraEnvironment.isEmpty {
+      result += " #"
+      for (envVar, val) in extraEnvironment {
+        result += " \(envVar)=\(val)"
       }
     }
 
