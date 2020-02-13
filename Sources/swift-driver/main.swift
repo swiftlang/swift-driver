0cfda3562e7d48e2069ae5172f2d8c5ba8546bdd
Robin Kunde <robin.kunde@recoursive.com>
2020-02-13 15:00:23 -0500

    Diagnose lack of input files

--- Sources/swift-driver/main.swift
+++ Sources/swift-driver/main.swift
@@ -52,6 +52,9 @@ do {
   exit(EXIT_FAILURE)
 } catch let diagnosticData as DiagnosticData {
   diagnosticsEngine.emit(.error(diagnosticData))
+} catch let driverError as Driver.Error {
+  print("error: \(driverError.localizedDescription)")
+  exit(EXIT_FAILURE)
 } catch {
   print("error: \(error)")
   exit(EXIT_FAILURE)
