697e4ad373fcbd0141729659994b3758893bf9d4
Owen Voorhees <owen@squibner.com>
2020-07-27 21:18:34 -0700

    Add utilities for reading the LLVM bitstream container format
    Originally authored by Jordan Rose
    Modified to drop some printing and logging code that's not needed for the driver's use cases
    
    Co-authored-by: Jordan Rose <jrose@belkadan.com>

--- Sources/SwiftDriver/Incremental Compilation/SourceFileDependencyGraph.swift
+++ Sources/SwiftDriver/Incremental Compilation/SourceFileDependencyGraph.swift
@@ -111,3 +111,111 @@ extension SourceFileDependencyGraph {
     }
   }
 }
+
+extension SourceFileDependencyGraph {
+  private static let recordBlockId = 8
+
+  private enum RecordKind: UInt64 {
+    case metadata = 1
+    case sourceFileDepGraphNode
+    case fingerprintNode
+    case dependsOnDefinitionNode
+    case identifierNode
+  }
+
+  private enum ReadError: Error {
+    case badMagic
+    case noRecordBlock
+    case malformedMetadataRecord
+    case unexpectedMetadataRecord
+    case malformedFingerprintRecord
+    case malformedDependsOnDefinitionRecord
+    case malformedIdentifierRecord
+    case malformedSourceFileDepGraphNodeRecord
+    case unknownRecord
+    case unexpectedSubblock
+  }
+
+  public init(data: Data) throws {
+    // FIXME: visit blocks and records incrementally instead of reading the
+    // entire file up front.
+    let bitcode = try Bitcode(data: data)
+    guard bitcode.signature == .init(string: "DEPS") else { throw ReadError.badMagic }
+
+    guard bitcode.elements.count == 1,
+          case .block(let recordBlock) = bitcode.elements.first,
+          recordBlock.id == Self.recordBlockId else { throw ReadError.noRecordBlock }
+
+    guard case .record(let metadataRecord) = recordBlock.elements.first,
+          RecordKind(rawValue: metadataRecord.id) == .metadata,
+          metadataRecord.fields.count == 2,
+          case .blob(let compilerVersionBlob) = metadataRecord.payload,
+          let compilerVersionString = String(data: compilerVersionBlob, encoding: .utf8)
+    else { throw ReadError.malformedMetadataRecord }
+
+    self.majorVersion = metadataRecord.fields[0]
+    self.minorVersion = metadataRecord.fields[1]
+    self.compilerVersionString = compilerVersionString
+
+    var nodes: [Node] = []
+    var node: Node? = nil
+    var identifiers: [String] = [""] // The empty string is hardcoded as identifiers[0]
+    var sequenceNumber = 0
+    for element in recordBlock.elements.dropFirst() {
+      guard case .record(let record) = element else { throw ReadError.unexpectedSubblock }
+      guard let kind = RecordKind(rawValue: record.id) else { throw ReadError.unknownRecord }
+      switch kind {
+      case .metadata:
+        throw ReadError.unexpectedMetadataRecord
+      case .sourceFileDepGraphNode:
+        if let node = node {
+          nodes.append(node)
+        }
+        guard record.fields.count == 5,
+              let nodeKind = DependencyKey.Kind(rawValue: record.fields[0]),
+              let declAspect = DeclAspect(rawValue: record.fields[1]),
+              record.fields[2] < identifiers.count,
+              record.fields[3] < identifiers.count else {
+          throw ReadError.malformedSourceFileDepGraphNodeRecord
+        }
+        let context = identifiers[Int(record.fields[2])]
+        let identifier = identifiers[Int(record.fields[3])]
+        let isProvides = record.fields[4] != 0
+        node = Node(key: .init(kind: nodeKind,
+                               aspect: declAspect,
+                               context: context,
+                               name: identifier),
+                    fingerprint: nil,
+                    sequenceNumber: sequenceNumber,
+                    defsIDependUpon: [],
+                    isProvides: isProvides)
+        sequenceNumber += 1
+      case .fingerprintNode:
+        guard node != nil,
+              record.fields.count == 0,
+              case .blob(let fingerprintBlob) = record.payload,
+              let fingerprint = String(data: fingerprintBlob, encoding: .utf8) else {
+          throw ReadError.malformedFingerprintRecord
+        }
+        node?.fingerprint = fingerprint
+      case .dependsOnDefinitionNode:
+        guard node != nil,
+              record.fields.count == 1 else { throw ReadError.malformedDependsOnDefinitionRecord }
+        node?.defsIDependUpon.append(Int(record.fields[0]))
+      case .identifierNode:
+        guard record.fields.count == 0,
+              case .blob(let identifierBlob) = record.payload,
+              let identifier = String(data: identifierBlob, encoding: .utf8) else {
+          throw ReadError.malformedIdentifierRecord
+        }
+        identifiers.append(identifier)
+      }
+    }
+
+    if let node = node {
+      nodes.append(node)
+    }
+
+    self.allNodes = nodes
+  }
+}
