f92010deac747e552bb6e736e18f5db36ddce020
Owen Voorhees <owen@squibner.com>
2020-08-09 12:14:42 -0700

    Support module wrapping for debugging on Linux

---
+++ Sources/SwiftDriver/Jobs/ModuleWrapJob.swift
@@ -0,0 +1,39 @@
+//===--------------- ModuleWrapJob.swift - Swift Module Wrapping ----------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+extension Driver {
+  mutating func moduleWrapJob(moduleInput: TypedVirtualPath) throws -> Job {
+    var commandLine: [Job.ArgTemplate] = swiftCompilerPrefixArgs.map { Job.ArgTemplate.flag($0) }
+
+    commandLine.appendFlags("-modulewrap")
+
+    // Add the input.
+    commandLine.append(.path(moduleInput.file))
+    assert(compilerOutputType == .object, "-modulewrap mode only produces object files")
+
+    commandLine.appendFlags("-target", targetTriple.triple)
+
+    let outputPath = try moduleInput.file.replacingExtension(with: .object)
+    commandLine.appendFlag("-o")
+    commandLine.appendPath(outputPath)
+
+    return Job(
+      moduleName: moduleOutputInfo.name,
+      kind: .moduleWrap,
+      tool: .absolute(try toolchain.getToolPath(.swiftCompiler)),
+      commandLine: commandLine,
+      inputs: [moduleInput],
+      outputs: [.init(file: outputPath, type: .object)],
+      supportsResponseFiles: true
+    )
+  }
+}
