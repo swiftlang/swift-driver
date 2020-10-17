f9229d210b0f6c7d2c10307dd9cce77b596b9029
Owen Voorhees <owen@squibner.com>
2020-10-16 20:23:04 -0700

    Add the module interface input to the command line of a verification job

--- Tests/SwiftDriverTests/SwiftDriverTests.swift
+++ Tests/SwiftDriverTests/SwiftDriverTests.swift
@@ -2643,6 +2643,7 @@ final class SwiftDriverTests: XCTestCase {
       XCTAssertEqual(verifyJob.kind, .verifyModuleInterface)
       XCTAssertTrue(verifyJob.inputs.count == 1)
       XCTAssertTrue(verifyJob.inputs[0] == mergeInterfaceOutputs[0])
+      XCTAssertTrue(verifyJob.commandLine.contains(.path(mergeInterfaceOutputs[0].file)))
     }
     // No Evolution
     do {
