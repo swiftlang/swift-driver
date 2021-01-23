bfb9460d3d0743c68e927bce30ad32cf07c54b64
David Ungar <dungar@apple.com>
2021-01-23 09:32:03 -0800

    Move subtraction to outer function to cope with multiple input jobs.

--- Sources/SwiftDriver/IncrementalCompilation/IncrementalCompilationState.swift
+++ Sources/SwiftDriver/IncrementalCompilation/IncrementalCompilationState.swift
@@ -565,6 +565,7 @@ extension IncrementalCompilationState {
           return Array(skippedCompileGroups.keys)
         }
       )
+      .subtracting(job.primaryInputs) // have already compiled these
     )
     .sorted {$0.file.name < $1.file.name}
   }
