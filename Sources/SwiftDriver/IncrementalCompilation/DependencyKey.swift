e11ba0fcdefa83a614681cae1e19a09f112f1874
Robert Widmann <devteam.codafi@gmail.com>
2021-01-23 14:31:11 -0800

    [Gardening] Redocument DependencyKey

--- Sources/SwiftDriver/IncrementalCompilation/DependencyKey.swift
+++ Sources/SwiftDriver/IncrementalCompilation/DependencyKey.swift
@@ -21,41 +21,124 @@ import Foundation
 }
 
 
-
+/// A `DependencyKey` carries all of the information necessary to uniquely
+/// identify a dependency node in the graph, and serves as a point of identity
+/// for the dependency graph's map from definitions to uses.
 public struct DependencyKey: Hashable, CustomStringConvertible {
-  /// Instead of the status quo scheme of two kinds of "Depends", cascading and
-  /// non-cascading this code represents each entity ("Provides" in the status
-  /// quo), by a pair of nodes. One node represents the "implementation." If the
-  /// implementation changes, users of the entity need not be recompiled. The
-  /// other node represents the "interface." If the interface changes, any uses of
-  /// that definition will need to be recompiled. The implementation always
-  /// depends on the interface, since any change that alters the interface will
-  /// require the implementation to be rebuilt. The interface does not depend on
-  /// the implementation. In the dot files, interfaces are yellow and
-  /// implementations white. Each node holds an instance variable describing which
+  /// Captures which facet of the dependency structure a dependency key represents.
+  ///
+  /// A `DeclAspect` is used to separate dependencies with a scope limited to
+  /// a single file (or declaration within a file) from dependencies with a
+  /// scope that may affect many files.
+  ///
+  /// Those dependencies that are localized to a single file or declaration are
+  /// a part of the "implementation" aspect. Changes to nodes with the
+  /// implementation aspect do not require uses of that node to be recompiled.
+  /// The Swift frontend models all uses of declarations in a file as part of
+  /// the implementation aspect.
+  ///
+  /// The remaining kind of dependencies are a part of the "interface" aspect.
+  /// Changes to nodes with the interface aspect *require* all uses of the node
+  /// to be rebuilt. The Swift frontend models the definitions of types,
+  /// functions, and members with an interface node.
+  ///
+  /// Special Considerations
+  /// ======================
+  ///
+  /// When the Swift frontend creates dependency nodes, they occur in
+  /// interface/implementation pairs. The idea being that the implementation
+  /// aspect node depends on the interface aspect node but, crucially, not
+  /// vice versa. This models the fact that changes to the interface of a node
+  /// must cause its implementation to be recompiled.
+  ///
+  /// Dumping Dependency Graphs
+  /// =========================
+  ///
+  /// When the driver's dependency graph is dumped as a dot file, nodes with
+  /// the interface aspect are yellow and nodes with the implementations aspect
+  /// are white. Each node holds an instance variable describing which
   /// aspect of the entity it represents.
-
   /*@_spi(Testing)*/ public enum DeclAspect: Comparable {
-    case interface, implementation
+    /// The "interface" aspect.
+    case interface
+    /// The "implementation" aspect.
+    case implementation
   }
 
-  /// Encode the current sorts of dependencies as kinds of nodes in the dependency
-  /// graph, splitting the current *member* into \ref member and \ref
-  /// potentialMember and adding \ref sourceFileProvide.
-  ///
+  /// Enumerates the current sorts of dependency nodes in the dependency graph.
   /*@_spi(Testing)*/ public enum Designator: Hashable, CustomStringConvertible {
-    case
-      topLevel(name: String),
-      dynamicLookup(name: String),
-      externalDepend(ExternalDependency),
-      sourceFileProvide(name: String)
-
-    case
-      nominal(context: String),
-      potentialMember(context: String)
-
-    case
-      member(context: String, name: String)
+    /// A top-level name.
+    ///
+    /// Corresponds to the top-level names that occur in a given file. When
+    /// a top-level name matching this name is added, removed, or modified
+    /// the corresponding dependency node will be marked for recompilation.
+    ///
+    /// The `name` parameter is the human-readable name of the top-level
+    /// declaration.
+    case topLevel(name: String)
+    /// A dependency originating from the lookup of a "dynamic member".
+    ///
+    /// A "dynamic member lookup" is the Swift frontend's term for lookups that
+    /// occur against instances of `AnyObject`. Because an `AnyObject` node
+    /// behaves like `id` in that it can recieve any kind of Objective-C
+    /// message, the compiler takes care to log these names separately.
+    ///
+    /// The `name` parameter is the human-readable base name of the Swift method
+    /// the dynamic member was imported as. e.g. `-[NSString initWithString:]`
+    /// appears as `init`.
+    ///
+    /// - Note: This is distinct from "dynamic member lookup", which uses
+    ///         a normal `member` constraint.
+    case dynamicLookup(name: String)
+    /// A dependency that resides outside of the module being built.
+    ///
+    /// These dependencies correspond to clang modules and their immediate
+    /// dependencies, header files imported via the bridging header, and Swift
+    /// modules that do not have embedded incremental dependency information.
+    /// Because of this, the Swift compiler and Driver have very little
+    /// information at their disposal to make scheduling decisions relative to
+    /// the other kinds of dependency nodes. Thus, when the modification time of
+    /// an external dependency node changes, the Driver is forced to rebuild all
+    /// uses of the dependency.
+    ///
+    /// The full path to the external dependency as seen by the frontend is
+    /// available from this node.
+    case externalDepend(ExternalDependency)
+    /// A source file - acts as the root for all dependencies provided by
+    /// declarations in that file.
+    ///
+    /// The `name` of the file is a path to the `swiftdeps` file named in
+    /// the output file map for a given Swift file.
+    case sourceFileProvide(name: String)
+    /// A "nominal" type that is used, or defined by this file.
+    ///
+    /// Unlike a top-level name, a `nominal` dependency always names exactly
+    /// one unique declaration (opp. many declarations with the same top-level
+    /// name). The `context` field of this type is the mangled name of this
+    /// type. When a component of the mangling for the nominal type changes,
+    /// the corresponding dependency node will be marked for recompilation.
+    /// These nodes generally capture the space of ABI-breaking changes made to
+    /// types themselves such as the addition or removal of generic parameters,
+    /// or a change in base name.
+    case nominal(context: String)
+    /// A "potential member" constraint models the abstract interface of a
+    /// particular type or protocol. They can be thought of as a kind of
+    /// "globstar" member constraint. Whenever a member is added, removed or
+    /// modified, or the type itself is deleted, the corresponding dependency
+    /// node will be marked for recompilation.
+    ///
+    /// Potential member nodes are used to model protocol conformances and
+    /// superclass constraints where the modification of members affects the
+    /// layout of subclasses or protocol conformances.
+    ///
+    /// Like `nominal` nodes, the `context` field is the mangled name of the
+    /// subject type.
+    case potentialMember(context: String)
+    /// A member of a type.
+    ///
+    /// The `context` field corresponds to the mangled name of the type. The
+    /// `name` field corresponds to the *unmangled* name of the member.
+    case member(context: String, name: String)
 
     var externalDependency: ExternalDependency? {
       switch self {
