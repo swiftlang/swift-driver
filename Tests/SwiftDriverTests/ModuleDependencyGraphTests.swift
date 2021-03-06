8a881690613800117381a0699c1dadccbbee9f48
David Ungar <dungar@apple.com>
2021-03-05 20:26:44 -0800

    Use _spi

--- Tests/SwiftDriverTests/ModuleDependencyGraphTests.swift
+++ Tests/SwiftDriverTests/ModuleDependencyGraphTests.swift
@@ -11,7 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 import XCTest
-@testable import SwiftDriver
+@_spi(Testing) import SwiftDriver
 import TSCBasic
 
 class ModuleDependencyGraphTests: XCTestCase {
