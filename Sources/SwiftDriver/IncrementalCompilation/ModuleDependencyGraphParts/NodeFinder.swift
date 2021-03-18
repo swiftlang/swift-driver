b3c280c89493cfea9a06f9223ca8659ac2bf57e0
David Ungar <dungar@apple.com>
2021-03-18 10:19:59 -0700

    Only verifyUseIsOK in a debug build.

--- Sources/SwiftDriver/IncrementalCompilation/ModuleDependencyGraphParts/NodeFinder.swift
+++ Sources/SwiftDriver/IncrementalCompilation/ModuleDependencyGraphParts/NodeFinder.swift
@@ -131,7 +131,7 @@ extension ModuleDependencyGraph.NodeFinder {
   
   /// record def-use, return if is new use
   mutating func record(def: DependencyKey, use: Graph.Node) -> Bool {
-    verifyUseIsOK(use)
+    assert(verifyUseIsOK(use))
     return usesByDef.insertValue(use, forKey: def)
   }
 }
