0998d090978307d33b6673b52955f5a2881c05fb
David Ungar <dungar@apple.com>
2021-03-21 14:51:07 -0700

    Cache hashes

--- Sources/SwiftDriver/IncrementalCompilation/DependencyKey.swift
+++ Sources/SwiftDriver/IncrementalCompilation/DependencyKey.swift
@@ -308,6 +308,7 @@ public struct DependencyKey: Hashable, CustomStringConvertible {
 
   /*@_spi(Testing)*/ public let aspect: DeclAspect
   /*@_spi(Testing)*/ public let designator: Designator
+  public let hashValue: Int
 
 
   /*@_spi(Testing)*/ public init(
@@ -316,6 +317,10 @@ public struct DependencyKey: Hashable, CustomStringConvertible {
   {
     self.aspect = aspect
     self.designator = designator
+    var h = Hasher()
+    h.combine(aspect)
+    h.combine(designator)
+    self.hashValue = h.finalize()
   }
 
 
