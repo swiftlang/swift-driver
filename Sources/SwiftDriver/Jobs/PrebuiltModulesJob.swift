68bd331a0656bace32ce8b7d9b47f275b9ae0959
Xi Ge <xi_ge@apple.com>
2021-05-07 10:52:35 -0700

    PrebuiltModuleGen: allow retrying after hitting bad file descriptor error

--- Sources/SwiftDriver/Jobs/PrebuiltModulesJob.swift
+++ Sources/SwiftDriver/Jobs/PrebuiltModulesJob.swift
@@ -267,6 +267,8 @@ extension Driver {
       commandLine.append(.path(try VirtualPath(path: mcp)))
     }
     commandLine.appendFlag(.serializeParseableModuleInterfaceDependencyHashes)
+    commandLine.appendFlag(.badFileDescriptorRetryCount)
+    commandLine.appendFlag("30")
     return Job(
       moduleName: moduleName,
       kind: .compile,
