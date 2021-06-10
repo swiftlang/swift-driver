a75096bab34a30f4896a002797b74e23421274e8
Keith Smiley <keithbsmiley@gmail.com>
2021-06-09 21:18:26 -0700

    Disable output file map diagnostic for non-incremental mode
    This re-implements this logic https://github.com/apple/swift/blob/3116eed2e465a2688f070b3b87adc106cbbaf09f/lib/Driver/Driver.cpp#L414-L417 to not warn when output file maps don't have a main entry when using WMO

--- Sources/SwiftDriver/Driver/Driver.swift
+++ Sources/SwiftDriver/Driver/Driver.swift
@@ -592,6 +592,7 @@ public struct Driver {
       fileSystem: fileSystem,
       moduleOutputInfo: moduleOutputInfo,
       outputFileMap: outputFileMap,
+      incremental: self.shouldAttemptIncrementalCompilation,
       parsedOptions: parsedOptions,
       recordedInputModificationDates: recordedInputModificationDates)
 
