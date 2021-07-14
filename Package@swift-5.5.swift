c895c55901b7214d45ece9d94e59a985e5a75a34
Robert Widmann <devteam.codafi@gmail.com>
2021-07-14 16:15:53 -0700

    Reference the yams Package as Yams
    The opposite of #754 because the local package URL is lowercase, but the
    actual repo is uppercase.

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -158,7 +158,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
 } else {
     package.dependencies += [
         .package(path: "../swift-tools-support-core"),
-        .package(path: "../yams"),
+        .package(name: "Yams", path: "../yams"),
         .package(path: "../swift-argument-parser"),
     ]
 }
