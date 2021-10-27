0c7aec6376b239efc2b5a21646cddd7eb4aaec93
Victoria Mitchell <victoria_m@apple.com>
2021-10-27 14:22:53 -0600

    update symbol graph emission flags

--- Sources/SwiftDriver/Jobs/CompileJob.swift
+++ Sources/SwiftDriver/Jobs/CompileJob.swift
@@ -354,6 +354,8 @@ extension Driver {
       try commandLine.appendLast(.emitSymbolGraph, from: &parsedOptions)
       try commandLine.appendLast(.emitSymbolGraphDir, from: &parsedOptions)
     }
+    try commandLine.appendLast(.includeSpiSymbols, from: &parsedOptions)
+    try commandLine.appendLast(.symbolGraphMinimumAccessLevel, from: &parsedOptions)
 
     addJobOutputs(outputs)
 
