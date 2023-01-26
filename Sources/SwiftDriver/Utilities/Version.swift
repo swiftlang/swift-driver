3903a1935c9ef8a90c3dd388487188b0132c0113
Saleem Abdulrasool <compnerd@compnerd.org>
2023-01-26 08:04:54 -0800

    Utilities: add missing file header
    Add a file copyright header to a newly minted file.

--- Sources/SwiftDriver/Utilities/Version.swift
+++ Sources/SwiftDriver/Utilities/Version.swift
@@ -1,3 +1,14 @@
+//===--------------- Version.swift - Version Handling Routines ------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 /// Errors related to a version string.
 public enum VersionError: Error {
