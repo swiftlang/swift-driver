698b14074b7587d4ac5c68c46851d17aa3ed6bc5
Artem Chikin <achikin@apple.com>
2023-12-01 13:10:15 -0800

    Avoid generating an interface verification job for a package interface when no such interface is emitted

--- Sources/SwiftDriver/Jobs/Planning.swift
+++ Sources/SwiftDriver/Jobs/Planning.swift
@@ -605,7 +605,9 @@ extension Driver {
     }
     try addVerifyJob(for: .Public)
     try addVerifyJob(for: .Private)
-    try addVerifyJob(for: .Package)
+    if parsedOptions.hasArgument(.packageName) {
+      try addVerifyJob(for: .Package)
+    }
   }
 
   private mutating func addAutolinkExtractJob(
