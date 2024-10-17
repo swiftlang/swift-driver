54b28a7f0671b573e8970a38ed9cc0cc5470cf37
Alex Hoppen <ahoppen@apple.com>
2024-10-17 14:22:03 -0700

    Output a Windows-style response file for swift-frontend and clang
    https://github.com/swiftlang/swift-driver/pull/1000 switched to use POSIX-style response files but swift-frontend expects Windows-style response files on Windows.
    
    Switch back to generating Windows-style response files on Windows and pass `--rsp-quoting=windows` to `clang` to tell it to pass the response file in Windows mode.
    
    Alternatives considered:
    - Have a separate response file generation logic for clang and Swift: This seems like a continuous source of bugs if you always need to think about which response file format you want, especially if the behavior is not intuitive (`clang.exe` expecting POSIX-style response files)
    - Instead of parsing `--rsp-quoting=windows` on Windows, use `clang-cl.exe`, which also causes response files to be parsed in Windows-style: I think it’s better to be explicit here instead of relying on clang’s implicit behavior based on which executable name was invoked.
    - Change swift-frontend to accept POSIX-style response files: swift-frontend and the old Swift driver share the same response file parsing logic. Changing swift-frontend without changing the old driver would require a bunch of flag parsing, which isn’t desirable. Also, using Windows-style response files on Windows seems like the better-fitting solution (CMake, for example, outputs Windows-style response files).
    
    Reverts https://github.com/swiftlang/swift-driver/pull/1000 with some logic added on top.

--- Tests/SwiftDriverTests/SwiftDriverTests.swift
+++ Tests/SwiftDriverTests/SwiftDriverTests.swift
@@ -1695,9 +1695,9 @@ final class SwiftDriverTests: XCTestCase {
       XCTAssertEqual(resolvedArgs[2].first, "@")
       let responseFilePath = try AbsolutePath(validating: String(resolvedArgs[2].dropFirst()))
       let contents = try localFileSystem.readFileContents(responseFilePath).description
-      XCTAssertTrue(contents.hasPrefix("\"-interpret\"\n\"/foo.swift\""))
-      XCTAssertTrue(contents.contains("\"-D\"\n\"TEST_20000\""))
-      XCTAssertTrue(contents.contains("\"-D\"\n\"TEST_1\""))
+      XCTAssertTrue(contents.hasPrefix("-interpret\n/foo.swift"))
+      XCTAssertTrue(contents.contains("-D\nTEST_20000"))
+      XCTAssertTrue(contents.contains("-D\nTEST_1"))
     }
 
     // Needs response file + disable override
@@ -1724,7 +1724,7 @@ final class SwiftDriverTests: XCTestCase {
       XCTAssertEqual(resolvedArgs[2].first, "@")
       let responseFilePath = try AbsolutePath(validating: String(resolvedArgs[2].dropFirst()))
       let contents = try localFileSystem.readFileContents(responseFilePath).description
-      XCTAssertTrue(contents.hasPrefix("\"-interpret\"\n\"/foo.swift\""))
+      XCTAssertTrue(contents.hasPrefix("-interpret\n/foo.swift"))
     }
 
     // No response file
