16688ae886a7bb80a283c593510e812af0d3c7c4
Artem Chikin <achikin@apple.com>
2025-03-28 16:06:04 -0700

    [Explicit Module Builds][Target Variant] Refactor build planning to allow for a second, separte emit-module task for the target variant
    This change allows builds that specify a variant triple and which must emit a target variant module to do so from within a single `swiftc` invocation when using Explicit Module Builds.
    
    This is an extensive refactor of the build planning procedure with respect to Explicit Module Builds:
    - Remove 'ExplicitDependencyBuildPlanner' global state from the 'Driver' instance
    - Instantiate a 'ExplicitDependencyBuildPlanner' instance at the beginning if build planning and propagate/thread this instance to all the required compilation task creation routines, all the way to 'addCommonFrontendOptions'
    - For '-target-variant' builds which emit a variant triple module, instantiate a whole separate explicit build planner, including performing a secondary dependency scan using the variant triple. Using this information:
      - Schedule variant triple module pre-compile dependency tasks
      - Schedule a variant triple PCH compilation task, if necessary
      - Schedule the variant emit-module task
      - Schedule the variant textual module verification task, if necessary

--- Tests/SwiftDriverTests/ExplicitModuleBuildTests.swift
+++ Tests/SwiftDriverTests/ExplicitModuleBuildTests.swift
@@ -169,19 +169,18 @@ func getStdlibShimsPaths(_ driver: Driver) throws -> (AbsolutePath, AbsolutePath
 final class ExplicitModuleBuildTests: XCTestCase {
   func testModuleDependencyBuildCommandGeneration() throws {
     do {
-      var driver = try Driver(args: ["swiftc", "-explicit-module-build",
+      let driver = try Driver(args: ["swiftc", "-explicit-module-build",
                                      "-module-name", "testModuleDependencyBuildCommandGeneration",
                                      "test.swift"])
       let moduleDependencyGraph =
             try JSONDecoder().decode(
               InterModuleDependencyGraph.self,
               from: ModuleDependenciesInputs.fastDependencyScannerOutput.data(using: .utf8)!)
-      driver.explicitDependencyBuildPlanner =
+      var explicitDependencyBuildPlanner =
         try ExplicitDependencyBuildPlanner(dependencyGraph: moduleDependencyGraph,
-                                           toolchain: driver.toolchain,
-                                           dependencyOracle: driver.interModuleDependencyOracle)
+                                           toolchain: driver.toolchain)
       let modulePrebuildJobs =
-        try driver.explicitDependencyBuildPlanner!.generateExplicitModuleDependenciesBuildJobs()
+        try explicitDependencyBuildPlanner.generateExplicitModuleDependenciesBuildJobs()
       XCTAssertEqual(modulePrebuildJobs.count, 4)
       for job in modulePrebuildJobs {
         XCTAssertEqual(job.outputs.count, 1)
@@ -216,56 +215,6 @@ final class ExplicitModuleBuildTests: XCTestCase {
             XCTFail("Unexpected module dependency build job output: \(job.outputs[0].file)")
         }
       }
-    }
-  }
-
-  func testModuleDependencyBuildCommandGenerationWithExternalFramework() throws {
-    do {
-      let externalDetails: ExternalTargetModuleDetailsMap =
-            [.swiftPrebuiltExternal("A"): ExternalTargetModuleDetails(path: try AbsolutePath(validating: "/tmp/A.swiftmodule"),
-                                                                      isFramework: true),
-             .swiftPrebuiltExternal("K"): ExternalTargetModuleDetails(path: try AbsolutePath(validating: "/tmp/K.swiftmodule"),
-                                                                       isFramework: true),
-             .swiftPrebuiltExternal("simpleTestModule"): ExternalTargetModuleDetails(path: try AbsolutePath(validating: "/tmp/simpleTestModule.swiftmodule"),
-                                                                                     isFramework: true)]
-      var driver = try Driver(args: ["swiftc", "-explicit-module-build",
-                                     "-module-name", "simpleTestModule",
-                                     "test.swift"])
-      var moduleDependencyGraph =
-            try JSONDecoder().decode(
-              InterModuleDependencyGraph.self,
-              from: ModuleDependenciesInputs.simpleDependencyGraphInput.data(using: .utf8)!)
-      // Key part of this test, using the external info to generate dependency pre-build jobs
-      try moduleDependencyGraph.resolveExternalDependencies(for: externalDetails)
-
-      // Ensure the main module was not overriden by an external dependency
-      XCTAssertNotNil(moduleDependencyGraph.modules[.swift("simpleTestModule")])
-
-      // Ensure the "K" module's framework status got resolved via `externalDetails`
-      guard case .swiftPrebuiltExternal(let kPrebuiltDetails) = moduleDependencyGraph.modules[.swiftPrebuiltExternal("K")]?.details else {
-        XCTFail("Expected prebuilt module details for module \"K\"")
-        return
-      }
-      XCTAssertTrue(kPrebuiltDetails.isFramework)
-      let jobsInPhases = try driver.computeJobsForPhasedStandardBuild(moduleDependencyGraph: moduleDependencyGraph)
-      let job = try XCTUnwrap(jobsInPhases.allJobs.first(where: { $0.kind == .compile }))
-      // Load the dependency JSON and verify this dependency was encoded correctly
-      XCTAssertJobInvocationMatches(job, .flag("-explicit-swift-module-map-file"))
-      let jsonDepsPathIndex = try XCTUnwrap(job.commandLine.firstIndex(of: .flag("-explicit-swift-module-map-file")))
-      let jsonDepsPathArg = job.commandLine[jsonDepsPathIndex + 1]
-      guard case .path(let jsonDepsPath) = jsonDepsPathArg else {
-        return XCTFail("No JSON dependency file path found.")
-      }
-      guard case let .temporaryWithKnownContents(_, contents) = jsonDepsPath else {
-        return XCTFail("Unexpected path type")
-      }
-      let dependencyInfoList = try JSONDecoder().decode(Array<SwiftModuleArtifactInfo>.self,
-                                                    from: contents)
-      XCTAssertEqual(dependencyInfoList.count, 2)
-      let dependencyArtifacts =
-        dependencyInfoList.first(where:{ $0.moduleName == "A" })!
-      // Ensure this is a framework, as specified by the externalDetails above.
-      XCTAssertEqual(dependencyArtifacts.isFramework, true)
     }
   }
 
@@ -430,10 +379,9 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                              "-working-directory", path.nativePathString(escaped: true),
                                              foo.nativePathString(escaped: true),
                                              "-emit-module", "-wmo", "-module-name", "Foo",
-                                             "-emit-module-path", FooInstallPath.nativePathString(escaped: true),
+                                             "-emit-module-path", FooInstallPath.appending(component: "Foo.swiftmodule").nativePathString(escaped: true),
                                              "-import-objc-header", fooHeader.nativePathString(escaped: true),
-                                             "-pch-output-dir", PCHPath.nativePathString(escaped: true),
-                                             FooInstallPath.appending(component: "Foo.swiftmodule").nativePathString(escaped: true)]
+                                             "-pch-output-dir", PCHPath.nativePathString(escaped: true)]
                                       + sdkArgumentsForTesting)
 
       let fooJobs = try fooBuildDriver.planBuild()
@@ -540,9 +488,8 @@ final class ExplicitModuleBuildTests: XCTestCase {
       if driver.isFrontendArgSupported(.scannerModuleValidation) {
         driver = try Driver(args: args + ["-scanner-module-validation"])
       }
-
       let _ = try driver.planBuild()
-      let dependencyGraph = try XCTUnwrap(driver.explicitDependencyBuildPlanner?.dependencyGraph)
+      let dependencyGraph = try XCTUnwrap(driver.intermoduleDependencyGraph)
 
       let checkForLinkLibrary = { (info: ModuleInfo, linkName: String, isFramework: Bool, shouldForceLoad: Bool) in
         let linkLibraries = try XCTUnwrap(info.linkLibraries)
@@ -624,7 +571,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
 
       let jobs = try driver.planBuild()
       // Figure out which Triples to use.
-      let dependencyGraph = try driver.gatherModuleDependencies()
+      let dependencyGraph = try driver.scanModuleDependencies()
       let mainModuleInfo = try dependencyGraph.moduleInfo(of: .swift("testExplicitModuleBuildJobs"))
       guard case .swift(_) = mainModuleInfo.details else {
         XCTFail("Main module does not have Swift details field")
@@ -892,7 +839,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
       }
       let jobs = try driver.planBuild()
       // Figure out which Triples to use.
-      let dependencyGraph = try driver.gatherModuleDependencies()
+      let dependencyGraph = try driver.scanModuleDependencies()
       let mainModuleInfo = try dependencyGraph.moduleInfo(of: .swift("testExplicitModuleVerifyInterfaceJobs"))
       guard case .swift(_) = mainModuleInfo.details else {
         XCTFail("Main module does not have Swift details field")
@@ -1027,7 +974,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
 
       let jobs = try driver.planBuild()
       // Figure out which Triples to use.
-      let dependencyGraph = try driver.gatherModuleDependencies()
+      let dependencyGraph = try driver.scanModuleDependencies()
       let mainModuleInfo = try dependencyGraph.moduleInfo(of: .swift("testExplicitModuleBuildPCHOutputJobs"))
       guard case .swift(_) = mainModuleInfo.details else {
         XCTFail("Main module does not have Swift details field")
@@ -1162,7 +1109,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
       XCTAssertJobInvocationMatches(interpretJob, .flag("-Xcc"), .flag("-fno-implicit-modules"))
 
       // Figure out which Triples to use.
-      let dependencyGraph = try driver.gatherModuleDependencies()
+      let dependencyGraph = try driver.scanModuleDependencies()
       let mainModuleInfo = try dependencyGraph.moduleInfo(of: .swift("testExplicitModuleBuildJobs"))
       guard case .swift(_) = mainModuleInfo.details else {
         XCTFail("Main module does not have Swift details field")
@@ -1295,7 +1242,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                      ] + sdkArgumentsForTesting)
 
       // Resulting graph should contain the real module name Bar
-      let dependencyGraphA = try driverA.gatherModuleDependencies()
+      let dependencyGraphA = try driverA.scanModuleDependencies()
       XCTAssertTrue(dependencyGraphA.modules.contains { (key: ModuleDependencyId, value: ModuleInfo) in
         key.moduleName == "Bar"
       })
@@ -1322,7 +1269,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                      ] + sdkArgumentsForTesting)
 
       // Resulting graph should contain the real module name Bar
-      let dependencyGraphB = try driverB.gatherModuleDependencies()
+      let dependencyGraphB = try driverB.scanModuleDependencies()
       XCTAssertTrue(dependencyGraphB.modules.contains { (key: ModuleDependencyId, value: ModuleInfo) in
         key.moduleName == "Bar"
       })
@@ -1369,7 +1316,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
       }
 
       // Resulting graph should contain the real module name Bar
-      let dependencyGraphA = try driverA.gatherModuleDependencies()
+      let dependencyGraphA = try driverA.scanModuleDependencies()
       XCTAssertTrue(dependencyGraphA.modules.contains { (key: ModuleDependencyId, value: ModuleInfo) in
         key.moduleName == "E"
       })
@@ -1395,7 +1342,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                      ] + sdkArgumentsForTesting)
 
       // Resulting graph should contain the real module name Bar
-      let dependencyGraphB = try driverB.gatherModuleDependencies()
+      let dependencyGraphB = try driverB.scanModuleDependencies()
       XCTAssertTrue(dependencyGraphB.modules.contains { (key: ModuleDependencyId, value: ModuleInfo) in
         key.moduleName == "E"
       })
@@ -2499,7 +2446,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                      "-explicit-module-build", "-module-name", "Test",
                                      "-module-cache-path", moduleCachePath.nativePathString(escaped: true),
                                      "-working-directory", path.nativePathString(escaped: true),
-                                     "-emit-module", outputModule.nativePathString(escaped: true),
+                                     "-emit-module", "-emit-module-path", outputModule.nativePathString(escaped: true),
                                      "-experimental-emit-module-separately",
                                      fileA.nativePathString(escaped: true), fileB.nativePathString(escaped: true)] + sdkArgumentsForTesting)
       let jobs = try driver.planBuild()
