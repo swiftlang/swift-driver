18e5109cbc8f765c0110346977018ad85d07659e
Artem Chikin <achikin@apple.com>
2025-05-08 13:15:39 -0700

    [Gardening] Deprecate global SwiftScan diagnostic gathering

--- Sources/SwiftDriver/ExplicitModuleBuilds/ModuleDependencyScanning.swift
+++ Sources/SwiftDriver/ExplicitModuleBuilds/ModuleDependencyScanning.swift
@@ -254,11 +254,7 @@ public extension Driver {
                                                              moduleAliases: moduleOutputInfo.aliases,
                                                              commandLine: command,
                                                              diagnostics: &scanDiagnostics)
-      } catch let DependencyScanningError.dependencyScanFailed(reason) {
-        try emitGlobalScannerDiagnostics()
-        throw DependencyScanningError.dependencyScanFailed(reason)
       }
-      try emitGlobalScannerDiagnostics()
     } else {
       // Fallback to legacy invocation of the dependency scanner with
       // `swift-frontend -scan-dependencies -import-prescan`
@@ -293,17 +289,6 @@ public extension Driver {
       }
   }
 
-  mutating internal func emitGlobalScannerDiagnostics() throws {
-    // We only emit global scanner-collected diagnostics as a legacy flow
-    // when the scanner does not support per-scan diagnostic output
-    guard try !interModuleDependencyOracle.supportsPerScanDiagnostics() else {
-      return
-    }
-    if let diags = try interModuleDependencyOracle.getScannerDiagnostics() {
-      try emitScannerDiagnostics(diags)
-    }
-  }
-
   mutating func performDependencyScan() throws -> InterModuleDependencyGraph {
     let scannerJob = try dependencyScanningJob()
     let forceResponseFiles = parsedOptions.hasArgument(.driverForceResponseFiles)
@@ -331,11 +316,7 @@ public extension Driver {
                                                                           commandLine: command,
                                                                           diagnostics: &scanDiagnostics)
         try emitScannerDiagnostics(scanDiagnostics)
-      } catch let DependencyScanningError.dependencyScanFailed(reason) {
-        try emitGlobalScannerDiagnostics()
-        throw DependencyScanningError.dependencyScanFailed(reason)
       }
-      try emitGlobalScannerDiagnostics()
     } else {
       // Fallback to legacy invocation of the dependency scanner with
       // `swift-frontend -scan-dependencies`
