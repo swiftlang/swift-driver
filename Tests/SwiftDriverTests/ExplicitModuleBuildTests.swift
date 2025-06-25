swiftlang/swift-driver

commit b0a01d3a283f6aa63ac64d0aa5af3d0cc8b64fc7

[Explicit Module Builds][Target Variant] Refactor build planning to allow for a second, separte emit-module task for the target variant
This change allows builds that specify a variant triple and which must emit a target variant module to do so from within a single `swiftc` invocation when using Explicit Module Builds.

This is an extensive refactor of the build planning procedure with respect to Explicit Module Builds:
- Remove 'ExplicitDependencyBuildPlanner' global state from the 'Driver' instance
- Instantiate a 'ExplicitDependencyBuildPlanner' instance at the beginning if build planning and propagate/thread this instance to all the required compilation task creation routines, all the way to 'addCommonFrontendOptions'
- For '-target-variant' builds which emit a variant triple module, instantiate a whole separate explicit build planner, including performing a secondary dependency scan using the variant triple. Using this information:
  - Schedule variant triple module pre-compile dependency tasks
  - Schedule a variant triple PCH compilation task, if necessary
  - Schedule the variant emit-module task
  - Schedule the variant textual module verification task, if necessary

Sources/SwiftDriver/Jobs/FrontendJobHelpers.swift
@@ -42,15 +42,6 @@ extension Driver {
     /// Use the precompiled bridging header.
     case precompiled
   }
-  /// Whether the driver has already constructed a module dependency graph or is in the process
-  /// of doing so
-  enum ModuleDependencyGraphUse {
-    /// Even though the driver may be in ExplicitModuleBuild mode, the dependency graph has not yet
-    /// been constructed, omit processing module dependencies
-    case dependencyScan
-    /// If the driver is in Explicit Module Build mode, the dependency graph has been computed
-    case computed
-  }
 
   /// If the given option is specified but the frontend doesn't support it, throw an error.
   func verifyFrontendSupportsOptionIfNecessary(_ option: Option) throws {
@@ -66,14 +57,20 @@ extension Driver {
     inputs: inout [TypedVirtualPath],
     kind: Job.Kind,
     bridgingHeaderHandling: BridgingHeaderHandling = .precompiled,
-    moduleDependencyGraphUse: ModuleDependencyGraphUse = .computed
+    explicitModulePlanner: ExplicitDependencyBuildPlanner? = nil,
+    forVariantEmitModule: Bool = false
   ) throws {
     // Only pass -target to the REPL or immediate modes if it was explicitly
     // specified on the command line.
     switch compilerMode {
     case .standardCompile, .singleCompile, .batchCompile, .compilePCM, .dumpPCM:
       commandLine.appendFlag(.target)
-      commandLine.appendFlag(targetTriple.triple)
+      if forVariantEmitModule,
+         let variant = parsedOptions.getLastArgument(.targetVariant)?.asSingle {
+        commandLine.appendFlag(Triple(variant, normalizing: true).triple)
+      } else {
+        commandLine.appendFlag(targetTriple.triple)
+      }
 
     case .repl, .immediate:
       if parsedOptions.hasArgument(.target) {
@@ -84,21 +81,21 @@ extension Driver {
       break
     }
 
-    let isPlanJobForExplicitModule = parsedOptions.contains(.driverExplicitModuleBuild) &&
-                                     moduleDependencyGraphUse == .computed
+    let isPlanJobForExplicitModule = parsedOptions.contains(.driverExplicitModuleBuild)
     let jobNeedPathRemap: Bool
     // If in ExplicitModuleBuild mode and the dependency graph has been computed, add module
     // dependencies.
     // May also be used for generation of the dependency graph itself in ExplicitModuleBuild mode.
-    if isPlanJobForExplicitModule {
+    if isPlanJobForExplicitModule,
+       let explicitModulePlanner = explicitModulePlanner {
       switch kind {
       case .generatePCH:
-        try addExplicitPCHBuildArguments(inputs: &inputs, commandLine: &commandLine)
+        explicitModulePlanner.resolveBridgingHeaderDependencies(inputs: &inputs, commandLine: &commandLine)
         jobNeedPathRemap = true
       case .compile, .emitModule, .interpret, .verifyModuleInterface:
-        try addExplicitModuleBuildArguments(inputs: &inputs, commandLine: &commandLine)
+        explicitModulePlanner.resolveMainModuleDependencies(inputs: &inputs, commandLine: &commandLine)
         jobNeedPathRemap = true
-      case .backend, .mergeModule, .compileModuleFromInterface,
+      case .backend, .compileModuleFromInterface,
            .generatePCM, .dumpPCM, .repl, .printTargetInfo,
            .versionRequest, .autolinkExtract, .generateDSYM,
            .help, .link, .verifyDebugInfo, .scanDependencies,
@@ -122,7 +119,8 @@ extension Driver {
       commandLine.appendFlag(flag)
     }
 
-    if let variant = parsedOptions.getLastArgument(.targetVariant)?.asSingle {
+    if !forVariantEmitModule,
+       let variant = parsedOptions.getLastArgument(.targetVariant)?.asSingle {
       commandLine.appendFlag(.targetVariant)
       commandLine.appendFlag(Triple(variant, normalizing: true).triple)
     }
@@ -473,9 +471,11 @@ extension Driver {
       try commandLine.appendAll(.Xcc, from: &parsedOptions)
     }
 
+    let (importedObjCHeader, precompiledObjCHeader) =
+              try computeCanonicalObjCHeader(explicitModulePlanner: explicitModulePlanner)
     let objcHeaderFile = (kind == .scanDependencies) ? originalObjCHeaderFile : importedObjCHeader
     if let importedObjCHeader = objcHeaderFile, bridgingHeaderHandling != .ignored {
-      if bridgingHeaderHandling == .precompiled, let pch = bridgingPrecompiledHeader {
+      if bridgingHeaderHandling == .precompiled, let pch = precompiledObjCHeader {
         // For explicit module build, we directly pass the compiled pch to
         // swift-frontend, rather than rely on swift-frontend to locate
         // the pch in the pchOutputDir and can start an implicit build in case
@@ -923,20 +923,6 @@ extension Driver {
     entries[inputEntry, default: [:]][output.type] = output.fileHandle
   }
 
-  /// Adds all dependencies required for an explicit module build
-  /// to inputs and command line arguments of a compile job.
-  mutating func addExplicitModuleBuildArguments(inputs: inout [TypedVirtualPath],
-                                                commandLine: inout [Job.ArgTemplate]) throws {
-    try explicitDependencyBuildPlanner?.resolveMainModuleDependencies(inputs: &inputs, commandLine: &commandLine)
-  }
-
-  /// Adds all dependencies required for an explicit module build of the bridging header
-  /// to inputs and command line arguments of a compile job.
-  mutating func addExplicitPCHBuildArguments(inputs: inout [TypedVirtualPath],
-                                             commandLine: inout [Job.ArgTemplate]) throws {
-    try explicitDependencyBuildPlanner?.resolveBridgingHeaderDependencies(inputs: &inputs, commandLine: &commandLine)
-  }
-
   mutating func addPluginPathArguments(commandLine: inout [Job.ArgTemplate]) throws {
     guard isFrontendArgSupported(.pluginPath) else {
       return
@@ -959,12 +945,6 @@ extension Driver {
 
     commandLine.appendFlag(.pluginPath)
     commandLine.appendPath(pluginPathRoot.localPluginPath)
-  }
-
-
-  /// If explicit dependency planner supports creating bridging header pch command.
-  public var supportsBridgingHeaderPCHCommand: Bool {
-    return explicitDependencyBuildPlanner?.supportsBridgingHeaderPCHCommand ?? false
   }
 
   /// In Explicit Module Build mode, distinguish between main module jobs and intermediate dependency build jobs,

Tests/SwiftDriverTests/ExplicitModuleBuildTests.swift
@@ -165,19 +165,18 @@ func getStdlibShimsPaths(_ driver: Driver) throws -> (AbsolutePath, AbsolutePath
 final class ExplicitModuleBuildTests: XCTestCase {
   func testModuleDependencyBuildCommandGeneration() throws {
     do {
-      var driver = try Driver(args: ["swiftc", "-explicit-module-build",
+      let driver = try Driver(args: ["swiftc", "-explicit-module-build",
                                      "-module-name", "testModuleDependencyBuildCommandGeneration",
                                      "test.swift"])
       let moduleDependencyGraph =
             try JSONDecoder().decode(
               InterModuleDependencyGraph.self,
               from: ModuleDependenciesInputs.fastDependencyScannerOutput.data(using: .utf8)!)
-      driver.explicitDependencyBuildPlanner =
+      var explicitDependencyBuildPlanner =
         try ExplicitDependencyBuildPlanner(dependencyGraph: moduleDependencyGraph,
-                                           toolchain: driver.toolchain,
-                                           dependencyOracle: driver.interModuleDependencyOracle)
+                                           toolchain: driver.toolchain)
       let modulePrebuildJobs =
-        try driver.explicitDependencyBuildPlanner!.generateExplicitModuleDependenciesBuildJobs()
+        try explicitDependencyBuildPlanner.generateExplicitModuleDependenciesBuildJobs()
       XCTAssertEqual(modulePrebuildJobs.count, 4)
       for job in modulePrebuildJobs {
         XCTAssertEqual(job.outputs.count, 1)
@@ -376,10 +375,9 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                              "-working-directory", path.nativePathString(escaped: true),
                                              foo.nativePathString(escaped: true),
                                              "-emit-module", "-wmo", "-module-name", "Foo",
-                                             "-emit-module-path", FooInstallPath.nativePathString(escaped: true),
+                                             "-emit-module-path", FooInstallPath.appending(component: "Foo.swiftmodule").nativePathString(escaped: true),
                                              "-import-objc-header", fooHeader.nativePathString(escaped: true),
-                                             "-pch-output-dir", PCHPath.nativePathString(escaped: true),
-                                             FooInstallPath.appending(component: "Foo.swiftmodule").nativePathString(escaped: true)]
+                                             "-pch-output-dir", PCHPath.nativePathString(escaped: true)]
                                       + sdkArgumentsForTesting)
 
       let fooJobs = try fooBuildDriver.planBuild()
@@ -607,9 +605,8 @@ final class ExplicitModuleBuildTests: XCTestCase {
       if driver.isFrontendArgSupported(.scannerModuleValidation) {
         driver = try Driver(args: args + ["-scanner-module-validation"])
       }
-
       let _ = try driver.planBuild()
-      let dependencyGraph = try XCTUnwrap(driver.explicitDependencyBuildPlanner?.dependencyGraph)
+      let dependencyGraph = try XCTUnwrap(driver.intermoduleDependencyGraph)
 
       let checkForLinkLibrary = { (info: ModuleInfo, linkName: String, isFramework: Bool, shouldForceLoad: Bool) in
         let linkLibraries = try XCTUnwrap(info.linkLibraries)
@@ -693,7 +690,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
 
       let jobs = try driver.planBuild()
       // Figure out which Triples to use.
-      let dependencyGraph = try driver.gatherModuleDependencies()
+      let dependencyGraph = try driver.scanModuleDependencies()
       let mainModuleInfo = try dependencyGraph.moduleInfo(of: .swift("testExplicitModuleBuildJobs"))
 
       guard case .swift(let mainModuleDetails) = mainModuleInfo.details else {
@@ -980,7 +977,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
       }
       let jobs = try driver.planBuild()
       // Figure out which Triples to use.
-      let dependencyGraph = try driver.gatherModuleDependencies()
+      let dependencyGraph = try driver.scanModuleDependencies()
       let mainModuleInfo = try dependencyGraph.moduleInfo(of: .swift("testExplicitModuleVerifyInterfaceJobs"))
       guard case .swift(_) = mainModuleInfo.details else {
         XCTFail("Main module does not have Swift details field")
@@ -1115,7 +1112,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
 
       let jobs = try driver.planBuild()
       // Figure out which Triples to use.
-      let dependencyGraph = try driver.gatherModuleDependencies()
+      let dependencyGraph = try driver.scanModuleDependencies()
       let mainModuleInfo = try dependencyGraph.moduleInfo(of: .swift("testExplicitModuleBuildPCHOutputJobs"))
       guard case .swift(_) = mainModuleInfo.details else {
         XCTFail("Main module does not have Swift details field")
@@ -1250,7 +1247,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
       XCTAssertJobInvocationMatches(interpretJob, .flag("-Xcc"), .flag("-fno-implicit-modules"))
 
       // Figure out which Triples to use.
-      let dependencyGraph = try driver.gatherModuleDependencies()
+      let dependencyGraph = try driver.scanModuleDependencies()
       let mainModuleInfo = try dependencyGraph.moduleInfo(of: .swift("testExplicitModuleBuildJobs"))
       guard case .swift(_) = mainModuleInfo.details else {
         XCTFail("Main module does not have Swift details field")
@@ -1383,7 +1380,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                      ] + sdkArgumentsForTesting)
 
       // Resulting graph should contain the real module name Bar
-      let dependencyGraphA = try driverA.gatherModuleDependencies()
+      let dependencyGraphA = try driverA.scanModuleDependencies()
       XCTAssertTrue(dependencyGraphA.modules.contains { (key: ModuleDependencyId, value: ModuleInfo) in
         key.moduleName == "Bar"
       })
@@ -1410,7 +1407,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                      ] + sdkArgumentsForTesting)
 
       // Resulting graph should contain the real module name Bar
-      let dependencyGraphB = try driverB.gatherModuleDependencies()
+      let dependencyGraphB = try driverB.scanModuleDependencies()
       XCTAssertTrue(dependencyGraphB.modules.contains { (key: ModuleDependencyId, value: ModuleInfo) in
         key.moduleName == "Bar"
       })
@@ -1457,7 +1454,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
       }
 
       // Resulting graph should contain the real module name Bar
-      let dependencyGraphA = try driverA.gatherModuleDependencies()
+      let dependencyGraphA = try driverA.scanModuleDependencies()
       XCTAssertTrue(dependencyGraphA.modules.contains { (key: ModuleDependencyId, value: ModuleInfo) in
         key.moduleName == "E"
       })
@@ -1483,7 +1480,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                      ] + sdkArgumentsForTesting)
 
       // Resulting graph should contain the real module name Bar
-      let dependencyGraphB = try driverB.gatherModuleDependencies()
+      let dependencyGraphB = try driverB.scanModuleDependencies()
       XCTAssertTrue(dependencyGraphB.modules.contains { (key: ModuleDependencyId, value: ModuleInfo) in
         key.moduleName == "E"
       })
@@ -2581,7 +2578,7 @@ final class ExplicitModuleBuildTests: XCTestCase {
                                      "-explicit-module-build", "-module-name", "Test",
                                      "-module-cache-path", moduleCachePath.nativePathString(escaped: true),
                                      "-working-directory", path.nativePathString(escaped: true),
-                                     "-emit-module", outputModule.nativePathString(escaped: true),
+                                     "-emit-module", "-emit-module-path", outputModule.nativePathString(escaped: true),
                                      "-experimental-emit-module-separately",
                                      fileA.nativePathString(escaped: true), fileB.nativePathString(escaped: true)] + sdkArgumentsForTesting)
       let jobs = try driver.planBuild()

Sources/SwiftDriver/Driver/Driver.swift
@@ -341,36 +341,13 @@ public struct Driver {
   /// Original ObjC Header passed from command-line
   let originalObjCHeaderFile: VirtualPath.Handle?
 
-
   /// Enable bridging header chaining.
   let bridgingHeaderChaining: Bool
-
-  /// The path to the imported Objective-C header.
-  lazy var importedObjCHeader: VirtualPath.Handle? = {
-    assert(explicitDependencyBuildPlanner != nil ||
-           !parsedOptions.hasArgument(.driverExplicitModuleBuild) ||
-           !inputFiles.contains { $0.type == .swift },
-           "should not be queried before scanning")
-    let chainedBridgingHeader = try? explicitDependencyBuildPlanner?.getChainedBridgingHeaderFile()
-    return try? computeImportedObjCHeader(&parsedOptions, compilerMode: compilerMode,
-                                          chainedBridgingHeader: chainedBridgingHeader) ?? originalObjCHeaderFile
-  }()
 
   /// The directory to emit PCH file.
   lazy var bridgingPrecompiledHeaderOutputDir: VirtualPath? = {
     return try? computePrecompiledBridgingHeaderDir(&parsedOptions,
                                                     compilerMode: compilerMode)
-  }()
-
-  /// The path to the pch for the imported Objective-C header.
-  lazy var bridgingPrecompiledHeader: VirtualPath.Handle? = {
-    let contextHash = try? explicitDependencyBuildPlanner?.getMainModuleContextHash()
-    return computeBridgingPrecompiledHeader(&parsedOptions,
-                                            compilerMode: compilerMode,
-                                            importedObjCHeader: importedObjCHeader,
-                                            outputFileMap: outputFileMap,
-                                            outputDirectory: bridgingPrecompiledHeaderOutputDir,
-                                            contextHash: contextHash)
   }()
 
   /// Path to the dependencies file.
@@ -620,14 +597,6 @@ public struct Driver {
 
   /// The mode the API digester should run in.
   let digesterMode: DigesterMode
-
-  // FIXME: We should soon be able to remove this from being in the Driver's state.
-  // Its only remaining use outside of actual dependency build planning is in
-  // command-line input option generation for the explicit main module compile job.
-  /// Planner for constructing module build jobs using Explicit Module Builds.
-  /// Constructed during the planning phase only when all module dependencies will be prebuilt and treated
-  /// as explicit inputs by the various compilation jobs.
-  @_spi(Testing) public var explicitDependencyBuildPlanner: ExplicitDependencyBuildPlanner? = nil
 
   /// A reference to the instance of libSwiftScan which is shared with the driver's
   /// `InterModuleDependencyOracle`, but also used for non-scanning tasks, such as target info
@@ -1383,9 +1352,9 @@ public struct Driver {
   }
 
   public mutating func planBuild() throws -> [Job] {
-    let (jobs, incrementalCompilationState, intermoduleDependencyGraph) = try planPossiblyIncrementalBuild()
+    let (jobs, incrementalCompilationState, explicitModulePlanner) = try planPossiblyIncrementalBuild()
     self.incrementalCompilationState = incrementalCompilationState
-    self.intermoduleDependencyGraph = intermoduleDependencyGraph
+    self.intermoduleDependencyGraph = explicitModulePlanner?.dependencyGraph
     return jobs
   }
 }
@@ -1774,11 +1743,9 @@ extension Diagnostic.Message {
 }
 
 extension Driver {
-  func explainModuleDependency(_ explainModuleName: String, allPaths: Bool) throws {
-    guard let dependencyPlanner = explicitDependencyBuildPlanner else {
-      fatalError("Cannot explain dependency without Explicit Build Planner")
-    }
-    guard let dependencyPaths = try dependencyPlanner.explainDependency(explainModuleName, allPaths: allPaths) else {
+  func explainModuleDependency(_ explainModuleName: String, allPaths: Bool,
+                               moduleDependencyGraph: InterModuleDependencyGraph) throws {
+    guard let dependencyPaths = try moduleDependencyGraph.explainDependency(explainModuleName, allPaths: allPaths) else {
       diagnosticEngine.emit(.remark("No such module dependency found: '\(explainModuleName)'"))
       return
     }
@@ -1846,13 +1813,6 @@ extension Driver {
         print(try executor.description(of: job, forceResponseFiles: forceResponseFiles))
       }
       return
-    }
-
-    // If we're only supposed to explain a dependency on a given module, do so now.
-    if let explainModuleName = parsedOptions.getLastArgument(.explainModuleDependencyDetailed) {
-      try explainModuleDependency(explainModuleName.asSingle, allPaths: true)
-    } else if let explainModuleNameDetailed = parsedOptions.getLastArgument(.explainModuleDependency) {
-      try explainModuleDependency(explainModuleNameDetailed.asSingle, allPaths: false)
     }
 
     if parsedOptions.contains(.driverPrintOutputFileMap) {
@@ -2047,7 +2007,7 @@ extension Driver {
 
     // Put bridging header as first input if we have it
     let allInputs: [TypedVirtualPath]
-    if let objcHeader = importedObjCHeader, bridgingPrecompiledHeader != nil {
+    if let objcHeader = originalObjCHeaderFile {
       allInputs = [TypedVirtualPath(file: objcHeader, type: .objcHeader)] + inputFiles
     } else {
       allInputs = inputFiles
@@ -2072,10 +2032,7 @@ extension Driver {
       // All input action IDs for this action.
       var inputIds = [UInt]()
 
-      var jobInputs = job.primaryInputs.isEmpty ? job.inputs : job.primaryInputs
-      if let pchPath = bridgingPrecompiledHeader, job.kind == .compile {
-        jobInputs.append(TypedVirtualPath(file: pchPath, type: .pch))
-      }
+      let jobInputs = job.primaryInputs.isEmpty ? job.inputs : job.primaryInputs
       // Collect input job IDs.
       for input in jobInputs {
         if let id = inputIdMap[input] {

Sources/SwiftDriver/ExplicitModuleBuilds/InterModuleDependencies/CommonDependencyOperations.swift
@@ -51,34 +51,6 @@ extension InterModuleDependencyGraph {
   }
 }
 
-@_spi(Testing) public extension InterModuleDependencyGraph {
-
-  /// Replace an existing module in the moduleInfoMap
-  static func replaceModule(originalId: ModuleDependencyId, replacementId: ModuleDependencyId,
-                            replacementInfo: ModuleInfo,
-                            in moduleInfoMap: inout ModuleInfoMap) {
-    precondition(moduleInfoMap[originalId] != nil)
-    moduleInfoMap.removeValue(forKey: originalId)
-    moduleInfoMap[replacementId] = replacementInfo
-    if originalId != replacementId {
-      updateDependencies(from: originalId, to: replacementId, in: &moduleInfoMap)
-    }
-  }
-
-  /// Replace all references to the original module in other externalModules' dependencies with the new module.
-  static func updateDependencies(from originalId: ModuleDependencyId,
-                                 to replacementId: ModuleDependencyId,
-                                 in moduleInfoMap: inout ModuleInfoMap) {
-    for moduleId in moduleInfoMap.keys {
-      var moduleInfo = moduleInfoMap[moduleId]!
-      if let originalModuleIndex = moduleInfo.directDependencies?.firstIndex(of: originalId) {
-        moduleInfo.directDependencies![originalModuleIndex] = replacementId;
-        moduleInfoMap[moduleId] = moduleInfo
-      }
-    }
-  }
-}
-
 /// Incremental Build Machinery
 internal extension InterModuleDependencyGraph {
   /// We must determine if any of the module dependencies require re-compilation
@@ -297,7 +269,7 @@ internal extension InterModuleDependencyGraph {
 }
 
 internal extension InterModuleDependencyGraph {
-  func explainDependency(dependencyModuleName: String, allPaths: Bool) throws -> [[ModuleDependencyId]]? {
+  func explainDependency(_ dependencyModuleName: String, allPaths: Bool) throws -> [[ModuleDependencyId]]? {
     guard modules.contains(where: { $0.key.moduleName == dependencyModuleName }) else { return nil }
     var result: Set<[ModuleDependencyId]> = []
     if allPaths {

Sources/SwiftDriver/ExplicitModuleBuilds/InterModuleDependencies/InterModuleDependencyGraph.swift
@@ -335,10 +335,10 @@ public extension ModuleInfo {
 /// all of the Swift and C modules and source files it depends on.
 public struct InterModuleDependencyGraph: Codable {
   /// The name of the main module.
-  public var mainModuleName: String
+  public let mainModuleName: String
 
   /// The complete set of modules discovered
-  public var modules: ModuleInfoMap = [:]
+  public let modules: ModuleInfoMap
 
   /// Information about the main module.
   public var mainModule: ModuleInfo { modules[.swift(mainModuleName)]! }

commit f7c585450c9b27b0d203765e63be7475498427ae

[Explicit Module Builds] Ensure that plugin paths are specified to the dependency scanning invocation
b0a01d3a283f6aa63ac64d0aa5af3d0cc8b64fc7 accidentally broke this by restricting these flags to never get passed when EBM is enabled, as opposed to ensuring they get passed on non-EBM and dependency scanning tasks.

Resolves rdar://154326377

Sources/SwiftDriver/Jobs/FrontendJobHelpers.swift
@@ -81,7 +81,7 @@ extension Driver {
       break
     }
 
-    let isPlanJobForExplicitModule = parsedOptions.contains(.driverExplicitModuleBuild)
+    let isPlanJobForExplicitModule = parsedOptions.contains(.driverExplicitModuleBuild) && explicitModulePlanner != nil
     let jobNeedPathRemap: Bool
     // If in ExplicitModuleBuild mode and the dependency graph has been computed, add module
     // dependencies.

Tests/SwiftDriverTests/ExplicitModuleBuildTests.swift
@@ -1987,6 +1987,52 @@ final class ExplicitModuleBuildTests: XCTestCase {
   }
 
   /// Test the libSwiftScan dependency scanning.
+  func testDependencyScanningPluginFlagPropagation() throws {
+    let (stdlibPath, shimsPath, toolchain, hostTriple) = try getDriverArtifactsForScanning()
+
+    // The dependency oracle wraps an instance of libSwiftScan and ensures thread safety across
+    // queries.
+    let dependencyOracle = InterModuleDependencyOracle()
+    let scanLibPath = try XCTUnwrap(toolchain.lookupSwiftScanLib())
+    try dependencyOracle.verifyOrCreateScannerInstance(swiftScanLibPath: scanLibPath)
+
+    // Create a simple test case.
+    try withTemporaryDirectory { path in
+      let main = path.appending(component: "testDependencyScanning.swift")
+      try localFileSystem.writeFileContents(main, bytes:
+        """
+        import C;\
+        import E;\
+        import G;
+        """
+      )
+
+      let cHeadersPath: AbsolutePath =
+      try testInputsPath.appending(component: "ExplicitModuleBuilds")
+        .appending(component: "CHeaders")
+      let swiftModuleInterfacesPath: AbsolutePath =
+      try testInputsPath.appending(component: "ExplicitModuleBuilds")
+        .appending(component: "Swift")
+      let sdkArgumentsForTesting = (try? Driver.sdkArgumentsForTesting()) ?? []
+      var driver = try Driver(args: ["swiftc",
+                                     "-I", cHeadersPath.nativePathString(escaped: true),
+                                     "-I", swiftModuleInterfacesPath.nativePathString(escaped: true),
+                                     "-I", stdlibPath.nativePathString(escaped: true),
+                                     "-I", shimsPath.nativePathString(escaped: true),
+                                     "/tmp/Foo.o",
+                                     "-plugin-path", "PluginA", "-external-plugin-path", "Plugin~B#Bexe",
+                                     "-explicit-module-build",
+                                     "-working-directory", path.nativePathString(escaped: true),
+                                     "-disable-clang-target",
+                                     main.nativePathString(escaped: true)] + sdkArgumentsForTesting)
+      let resolver = try ArgsResolver(fileSystem: localFileSystem)
+      var scannerCommand = try driver.dependencyScannerInvocationCommand().1.map { try resolver.resolve($0) }
+      XCTAssertTrue(scannerCommand.contains("-plugin-path"))
+      XCTAssertTrue(scannerCommand.contains("-external-plugin-path"))
+    }
+  }
+
+  /// Test the libSwiftScan dependency scanning.
   func testDependencyScanning() throws {
     let (stdlibPath, shimsPath, toolchain, hostTriple) = try getDriverArtifactsForScanning()
 