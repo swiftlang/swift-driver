58a7291b8d7130c0783a6d67f2e2175244e40919
Owen Voorhees <ovoorhees@apple.com>
2026-09-21 08:51:51 -0700

    Disable top level code inference when building swift-driver executable

---
+++ Sources/swift-driver/empty.swift
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2014 - 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+// Intentionally left blank so entrypoint.swift is not inferred as containing
+// top level code.
